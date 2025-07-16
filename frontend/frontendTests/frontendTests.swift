8a0a4fd83495fc615f4ae64052f2082ec63f5ec3
Mark Kulach <mark@markkulach.com>
2025-07-16 12:40:10 -0500

    initial commit

---
+++ frontend/frontendTests/frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  frontendTests.swift
+//  frontendTests
+//
+//  Created by Mark Kulach on 7/16/25.
+//
+
+import Testing
+@testable import frontend
+
+struct frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

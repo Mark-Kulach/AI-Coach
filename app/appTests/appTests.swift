2f11cac64da97e565583c2e75a7752cdb3736325
Mark Kulach <mark@markkulach.com>
2025-07-21 17:16:25 -0500

    initial commit

---
+++ app/appTests/appTests.swift
@@ -0,0 +1,17 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by Mark Kulach on 7/16/25.
+//
+
+import Testing
+@testable import app
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

b4ada3ee2ed946f90e552d5c7709c1ea782bb9b0
Mark Kulach <mark@markkulach.com>
2025-07-26 16:14:54 -0500

    Setup error logging

--- api/main.py
+++ api/main.py
@@ -1,20 +1,25 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,
 from google.cloud import storage
-
+from fastapi.responses import JSONResponse
+import logging
 app = FastAPI()
 
 BUCKET_NAME = "training-footage"
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
-    client = storage.Client()
-    bucket = client.bucket(BUCKET_NAME)
-    blob = bucket.blob(file.filename)
+    try:
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(file.filename)
 
-    blob.upload_from_file(file.file, content_type=file.content_type)
+        blob.upload_from_file(file.file, content_type=file.content_type)
 
-    return {
-        "message": "Upload successful",
-        "filename": file.filename,
-        "content_type": file.content_type
-    }
+        return {
+            "message": "Upload successful",
+            "filename": file.filename,
+            "content_type": file.content_type
+        }
+    except Exception as e:
+        logging.exception("Upload failed")
+        return JSONResponse(status_code=500, content={"error": str(e)})
